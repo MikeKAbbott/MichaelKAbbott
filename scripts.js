634d16e60a16066a5109d925e5b53808d00b0d9a
Michael Abbott <mikekabbott@gmail.com>
2021-05-20 14:40:39 -0400

    added images

--- scripts.js
+++ scripts.js
@@ -23,28 +23,3 @@ function draw() {
   fill(c);
   rect(width/2 - 20, height/2, 6, 50);
 }
-
-
-
-const phrases = ["I'm a coder!", "I'm a musician!", "I'm a proud cat dad!", "And a biker!"];
-let ul = document.getElementById("about-list");
-let i = 0;
-function addPhrases(){
-  setTimeout(function(){
-    ul.innerHTML += "<li><p>" + phrases[i] + "</p></li>";
-    i++;
-    if(i < phrases.length){
-      addPhrases();
-    }
-  },1000)
-  
-  
-}
-
-setInterval(() => {
-  i = 0;
-  ul.innerHTML = "";
-  addPhrases();
-
-}, 5000);
-
